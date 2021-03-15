61af58cd2c0f2f181b9b8227f1c34860cc6c2897
AndrewBeznosko <andrew.beznosko@gmail.com>
2021-03-15 22:11:21 +0200

    2nd task: sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,18 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let newArr = arr.slice();
+    newArr.sort(function (a, b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if( a == b) return -1;
 
+        return a.localeCompare(b);
+    });
+
+    if (param === 'desc') {
+        return newArr.reverse();      
+    } else {
+        return newArr;
+    } 
 }
